c7b796318b5cb251a8704f01c1ba103fb3e29c2b
PatrikBeichner <patrik.beichner@gmail.com>
2020-11-25 10:55:16 -0500

    Week 2 workshop update

---
+++ src/components/CampsiteInfoComponents.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+
+class CampsiteInfo extends React.Component {
+  state = {
+    toggle: true,
+  };
+
+  renderCampsite(campsite) {
+    return (
+      <div className="col-md-5 m-1">
+        <Card>
+          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+
+  renderComments(comments) {
+    if (this.state.toggle) {
+      if (comments) {
+        return (
+          <div className="col-md-5 m-1">
+            <h4>Comments</h4>
+            {comments.map((comment) => {
+              return (
+                <div key={comment.id}>
+                  <div>{comment.text}</div>
+                  <div>
+                    --{comment.author}, {new Intl.DateTimeFormat("en-US", { year: "numeric", month: "short", day: "2-digit" }).format(new Date(Date.parse(comment.date)))}
+                  </div>
+                </div>
+              );
+            })}
+            <button onClick={() => this.setState({ toggle: !this.state.toggle })}>Toggle comments</button>
+          </div>
+        );
+      } else {
+        return <div></div>;
+      }
+    } else {
+      return (
+        <div>
+          <button onClick={() => this.setState({ toggle: !this.state.toggle })}>Toggle comments</button>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    if (this.props.campsite) {
+      return (
+        <div className="row">
+          {this.renderCampsite(this.props.campsite)}
+          {this.renderComments(this.props.campsite.comments)}
+          <div className="col">{/* <button variant="primary">Toggle comments</button>{" "} */}</div>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+}
+export default CampsiteInfo;
