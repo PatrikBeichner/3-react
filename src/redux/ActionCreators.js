90fe41704997c2a165dbbec0b64722796d0d3121
PatrikBeichner <patrik.beichner@gmail.com>
2020-12-11 14:27:49 -0500

    Fetch handling errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -15,10 +15,31 @@ export const addComment = (campsiteId, rating, author, text) => ({
 //two arrows = arrow function nested in arrow function, able to do from thunk
 export const fetchCampsites = () => dispatch => {
     dispatch(campsitesLoading());
-    
+        //fetch request to server at baseUrl for campsites resource, returns promise for array of comments
     return fetch(baseUrl + 'campsites')
+        .then(response => {
+                //if response is in ok range (200-299) true if bad response (eg 404) false
+                if (response.ok) {
+                    return response;
+                } else {
+                    //status and statusText built in properties to make error more informative
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            //rejected promise error (no response from server)
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            }
+        )
+                //use then to access that array as the response if successful. .json method converts json to javascript array
         .then(response => response.json())
-        .then(campsites => dispatch(addCampsites(campsites)));
+                //if successful dispatch comments to be added to redux store
+        .then(campsites => dispatch(addCampsites(campsites)))
+                //catches errors when thrown or when other promises in chain rejected
+        .catch(error => dispatch(campsitesFailed(error.message)));
 
     // timeout to simulate lag while fetching from server
     // setTimeout(() => {
@@ -41,12 +62,24 @@ export const addCampsites = campsites => ({
 });
 
 export const fetchComments = () => dispatch => {
-    //fetch request to server at baseUrl for comments resource, returns promise for array of comments
     return fetch(baseUrl + 'comments')
-        //use then to access that array as the response if successful. .json method converts json to javascript array
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            }
+        )
         .then(response => response.json())
-        //if successful dispatch comments to be added to redux store
-        .then(comments => dispatch(addComments(comments)));
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
 };
 
 export const commentsFailed = errMess => ({
@@ -63,8 +96,23 @@ export const fetchPromotions = () => dispatch => {
     dispatch(promotionsLoading());
     
     return fetch(baseUrl + 'promotions')
+        .then(response => {
+                if (response.ok) {
+                    return response;
+                } else {
+                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+                }
+            },
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            }
+        )
         .then(response => response.json())
-        .then(promotions => dispatch(addPromotions(promotions)));
+        .then(promotions => dispatch(addPromotions(promotions)))
+        .catch(error => dispatch(promotionsFailed(error.message)));
 };
 
 export const promotionsLoading = () => ({
