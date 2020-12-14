41dc46ddde7a04beb863225f27d16341cef44884
PatrikBeichner <patrik.beichner@gmail.com>
2020-12-14 17:28:11 -0500

    partners.js tidy up

--- src/redux/partners.js
+++ src/redux/partners.js
@@ -14,16 +14,3 @@ export const Partners = (state = { isLoading: true, errMess: null, partners: [] 
   }
 };
 
-export const Promotions = (state = { isLoading: true, errMess: null, promotions: [] }, action) => {
-  switch (action.type) {
-    case ActionTypes.ADD_PROMOTIONS:
-      return { ...state, isLoading: false, errMess: null, promotions: action.payload };
-    case ActionTypes.PROMOTIONS_LOADING:
-      //is loading, no errors, promotions empty array because not loaded yet
-      return { ...state, isLoading: true, errMess: null, promotions: [] };
-    case ActionTypes.PROMOTIONS_FAILED:
-      return { ...state, isLoading: false, errMess: action.payload };
-    default:
-      return state;
-  }
-};
